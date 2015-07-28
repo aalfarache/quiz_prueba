d62511112ad20aa808cfb0bf33118e4f89bb5169
aalfarache <aal4004@gmail.com>
2015-07-28 18:49:49 +0200

    Mod 7 - Se añade BD: sequelize.js y SQLite

---
+++ models/models.js
@@ -0,0 +1,31 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite:
+var sequelize = new Sequelize(null, null, null,
+      { dialect: "sqlite",
+        storage: "quiz.sqlite"
+      }
+);
+
+// Importar la definición de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+// Exportar definición de tabla Quiz
+exports.Quiz = Quiz;
+
+// sequelize.sync() crea e inicializa tabla de preguntas en BD
+sequelize.sync().success(function() {
+  // success(...) ejecuta el manejador una vez creada la tabla
+  Quiz.count().success(function(count) {
+    if (count === 0) { //la tabla se inicializa sólo si está vacía
+      Quiz.create(
+        { pregunta: 'Capital de Italialllllllllllll',
+          respuesta: 'Romalllllllll'
+        }
+      ).success(function() {console.log('Base de datos inicializada')});
+    };
+  });
+});
