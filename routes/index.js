d7422116a76642458ae6d4b2d793e2e75df33910
aalfarache <aal4004@gmail.com>
2015-08-08 21:02:49 +0200

    Mod 9 - Autenticación y Sesión

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var authorController = require('../controllers/author_controller');
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,11 @@ router.get('/', function(req, res) {
 });
 
 router.param('quizId', quizController.load); //autoload :quizId
+
+/* GET session page. */
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 /* GET quizes page. */
 router.get('/quizes',                       quizController.index);
